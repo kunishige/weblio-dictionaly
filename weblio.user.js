dddebc676e05adef578caf6e75feeeb077d1c3d2
kunishige <kenagumi@gmail.com>
2012-10-20 13:09:22 +0900

    inisial commit

---
+++ weblio.user.js
@@ -0,0 +1,15 @@
+﻿// ==UserScript==
+// @name dictionaly
+// @namespace local
+// @description 
+// @include http://ejje.weblio.jp/content/*
+// ==/UserScript==
+
+function getword(){
+	var url  = location.href.split("/");
+	return url[(url.length)-1];
+}
+
+localStorage.setItem(getword(),JSON.parse(localStorage.getItem(getword()))+1);
+document.getElementById("logoBarR").innerHTML=(JSON.parse(localStorage.getItem(getword())))+"回目の検索";
+
